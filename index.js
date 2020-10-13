0cda2b8a38ba14a69dc5873eb238cf97939e5642
Jaakko Laiho <jaakkolaiho94@gmail.com>
2020-10-13 13:29:36 +0200

    increase request size limit attempt2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express       = require("express");
 const bodyParser = require('body-parser');
 const app           = express();
-app.use(bodyParser.json());
-app.use(bodyParser({limit: '5mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended:true}));
 
 
 app.use(function(req, res, next) {
